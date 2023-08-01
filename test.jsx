16730d64d41463c1e3909cdd241eb159cd0e5744
liaoyali-0216 <liaoyali@bytedance.com>
2023-08-01 16:17:54 +0800

    fix: 修改文件名称为Message

--- test.jsx
+++ test.jsx
@@ -1,17 +1,8 @@
-import { Button } from 'antd'
+import { message } from 'antd'
 
-import { Divider, Tabs, TabPane } from '@douyinfe/semi-ui'
-// const { TabPane } = Tabs
 
 function Test() {
-  return (
-    <Tabs>
-      <TabPane tab="项目 1" itemKey="item-1">
-        内容 1
-      </TabPane>
-      <TabPane tab="项目 2" itemKey="item-2">
-        内容 2
-      </TabPane>
-    </Tabs>
-  )
+ 
+  message.error('123');
+  message.error('123');
 }
